08528e3875a659522e8dcca1e44db1fa10782c9c
watarukura <what.r.j150@gmail.com>
2019-05-30 10:07:54 +0900

    use black for formatting

--- src/fizz_buzz_plus.py
+++ src/fizz_buzz_plus.py
@@ -1,16 +1,21 @@
 class FizzBuzzPlus:
-
     @staticmethod
     def answer(number: int) -> str:
-        text = ''
+        text = ""
         if number % 3 == 0:
-            text += 'Fizz'
+            text += "Fizz"
         if number % 5 == 0:
-            text += 'Buzz'
-        if str(number).find('3') > -1:
-            text += 'Fizz'
-        if str(number).find('5') > -1:
-            text += 'Buzz'
-        if text == '':
+            text += "Buzz"
+        if str(number).find("3") > -1:
+            text += "Fizz"
+        if str(number).find("5") > -1:
+            text += "Buzz"
+        if text == "":
             text = str(number)
         return text
+
+
+if __name__ == "__main__":
+    fizz_or_buzz_or_other = FizzBuzzPlus()
+    for i in range(1, 101):
+        print(fizz_or_buzz_or_other.answer(i))
---
+++ tests/unit/test_fizz_buzz_plus.py
@@ -0,0 +1,14 @@
+from src.fizz_buzz_plus import FizzBuzzPlus
+
+
+def test_fizz_buzz():
+    """a simple example to start you off"""
+    fizz_or_buzz_or_other = FizzBuzzPlus()
+    assert fizz_or_buzz_or_other.answer(3).find("Fizz") > -1
+    assert fizz_or_buzz_or_other.answer(9).find("Fizz") > -1
+    assert fizz_or_buzz_or_other.answer(31).find("Fizz") > -1
+    assert fizz_or_buzz_or_other.answer(38).find("Fizz") > -1
+    assert fizz_or_buzz_or_other.answer(10).find("Buzz") > -1
+    assert fizz_or_buzz_or_other.answer(51).find("Buzz") > -1
+    assert fizz_or_buzz_or_other.answer(58).find("Buzz") > -1
+    assert fizz_or_buzz_or_other.answer(7) == "7"
