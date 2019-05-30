b26d8c6fa8e2b45a03dc72ff05b3f62c5f189ea0
watarukura <what.r.j150@gmail.com>
2019-05-30 13:11:14 +0900

    test update for string match

--- tests/unit/test_fizz_buzz_plus.py
+++ tests/unit/test_fizz_buzz_plus.py
@@ -4,11 +4,11 @@ from src.fizz_buzz_plus import FizzBuzzPlus
 def test_fizz_buzz():
     """a simple example to start you off"""
     fizz_or_buzz_or_other = FizzBuzzPlus()
-    assert fizz_or_buzz_or_other.answer(3).find("Fizz") > -1
-    assert fizz_or_buzz_or_other.answer(9).find("Fizz") > -1
-    assert fizz_or_buzz_or_other.answer(31).find("Fizz") > -1
-    assert fizz_or_buzz_or_other.answer(38).find("Fizz") > -1
-    assert fizz_or_buzz_or_other.answer(10).find("Buzz") > -1
-    assert fizz_or_buzz_or_other.answer(51).find("Buzz") > -1
-    assert fizz_or_buzz_or_other.answer(58).find("Buzz") > -1
-    assert fizz_or_buzz_or_other.answer(7) == "7"
+    assert "Fizz" in fizz_or_buzz_or_other.answer(3)
+    assert "Fizz" in fizz_or_buzz_or_other.answer(9)
+    assert "Fizz" in fizz_or_buzz_or_other.answer(31)
+    assert "Fizz" in fizz_or_buzz_or_other.answer(38)
+    assert "Buzz" in fizz_or_buzz_or_other.answer(10)
+    assert "Buzz" in fizz_or_buzz_or_other.answer(51)
+    assert "Buzz" in fizz_or_buzz_or_other.answer(58)
+    assert "7" in fizz_or_buzz_or_other.answer(7)
