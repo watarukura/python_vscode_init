60ca142a97edbb035d622dca3cdc666ab6d1c294
wataru-kurashima <wataru-kurashima@hands-lab.com>
2019-06-08 22:14:03 +0900

    use black

--- tests/unit/test_fizz_buzz_plus.py
+++ tests/unit/test_fizz_buzz_plus.py
@@ -2,13 +2,11 @@ from src.fizz_buzz_plus import FizzBuzzPlus
 
 
 def test_fizz_buzz():
-    """a simple example to start you off"""
-    fizz_or_buzz_or_other = FizzBuzzPlus()
-    assert "Fizz" in fizz_or_buzz_or_other.answer(3)
-    assert "Fizz" in fizz_or_buzz_or_other.answer(9)
-    assert "Fizz" in fizz_or_buzz_or_other.answer(31)
-    assert "Fizz" in fizz_or_buzz_or_other.answer(38)
-    assert "Buzz" in fizz_or_buzz_or_other.answer(10)
-    assert "Buzz" in fizz_or_buzz_or_other.answer(51)
-    assert "Buzz" in fizz_or_buzz_or_other.answer(58)
-    assert "7" in fizz_or_buzz_or_other.answer(7)
+    assert "Fizz" in FizzBuzzPlus().answer(3)
+    assert "Fizz" in FizzBuzzPlus().answer(9)
+    assert "Fizz" in FizzBuzzPlus().answer(31)
+    assert "Fizz" in FizzBuzzPlus().answer(38)
+    assert "Buzz" in FizzBuzzPlus().answer(10)
+    assert "Buzz" in FizzBuzzPlus().answer(51)
+    assert "Buzz" in FizzBuzzPlus().answer(58)
+    assert "7" in FizzBuzzPlus().answer(7)
